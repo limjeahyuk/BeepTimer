c950231fc0547d5308f75d117ce0d64ca89b7b77
limjeahyuk <lim0202jh@gmail.com>
2025-08-02 17:44:12 +0900

    feat: 첫시작

---
+++ BeepTimer/BeepTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeepTimerApp.swift
+//  BeepTimer
+//
+//  Created by 임재혁 on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BeepTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
